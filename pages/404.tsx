0096608f30ec5f2876a20f2ed490cf40f720bc2c
João Otávio <joaotavioos@outlook.com>
2022-02-05 13:22:11 -0300

    Add notfound, review and newReview pages

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import { Layout, NotFound } from '@components'
+
+export default function NotFoundPage() {
+  return (
+    <Layout title="Page Not Found">
+      <NotFound />
+    </Layout>
+  )
+}
